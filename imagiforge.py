aa7b7ebd6688746f74377ae67427c8ae62e54ada
VolatileX1 <128452078+VolatileX1@users.noreply.github.com>
2023-07-08 12:21:02 +0530

    Adding crop and rotate features

--- imagiforge.py
+++ imagiforge.py
@@ -1,22 +1,26 @@
 from PIL import Image, ImageEnhance
 
 def change_aspect_ratio(image):
-    aspect_ratio = input("Enter the new aspect ratio (width:height): ").split(':')
-    width, height = map(int, aspect_ratio)
-    resized_image = image.resize((width, height))
-    return resized_image
+    width, height = map(int, input("Enter the new aspect ratio (width:height): ").split(':'))
+    return image.resize((width, height))
 
 def change_brightness(image):
     brightness = float(input("Enter the brightness value (-1.0 to 1.0): "))
     enhancer = ImageEnhance.Brightness(image)
-    adjusted_image = enhancer.enhance(brightness)
-    return adjusted_image
+    return enhancer.enhance(brightness)
 
 def change_contrast(image):
     contrast = float(input("Enter the contrast value (0.0 to 2.0): "))
     enhancer = ImageEnhance.Contrast(image)
-    adjusted_image = enhancer.enhance(contrast)
-    return adjusted_image
+    return enhancer.enhance(contrast)
+
+def crop_image(image):
+    left, upper, right, lower = map(int, input("Enter the crop region (left, upper, right, lower): ").split())
+    return image.crop((left, upper, right, lower))
+
+def rotate_image(image):
+    angle = float(input("Enter the rotation angle in degrees: "))
+    return image.rotate(angle)
 
 def save_image(image, filename):
     image.save(filename)
@@ -28,12 +32,14 @@ def main():
     operations = {
         1: change_aspect_ratio,
         2: change_brightness,
-        3: change_contrast
+        3: change_contrast,
+        4: crop_image,
+        5: rotate_image
     }
 
     print("Available operations:")
     for key, operation in operations.items():
-        print(f"{key}. {operation.__name__.replace('_', ' ').title()}")
+        print(f"{key}. {operation.__name__}")
 
     operation_choice = int(input("Enter the number corresponding to the desired operation: "))
 
